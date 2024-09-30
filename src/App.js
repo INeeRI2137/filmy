d0d86113369c644f993acd93ad5982365ba0791d
DESKTOP-NV760G3\3TP <poczta.oskara@gmail.com>
2024-09-30 13:00:19 +0200

    instalacja bootstrap, obsłucha funkcji, formularz z 2 polami, zbindowanie funkcji onSubmit,

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-
+import "bootstrap/dist/css/bootstrap.css";
 import './App.css';
 
 function App() {
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    const tytul = event.target.elements.tytul.value;
+    const rodzaj = event.target.elements.rodzaj.value;
+    console.log(`tytul: ${tytul}, rodzaj: ${rodzaj}`);
+  }
+
   return (
-    <div className="App">
-      <form>
-        <div class="mb-3">
-          <label for="tytul" class="form-label">Tytuł filmu</label>
-          <input type="text" class="form-control" id="tytul" /> [(ngModel)]="tytul" name="tytul">
+    <div className="container-fluid">
+      <form onSubmit={onSubmit}>
+        <div className="col-6">
+          <label htmlFor="tytul" className="form-label">Tytuł filmu</label>
+          <input type="text" className="form-control" id="tytul" placeholder="" />
         </div>
-        <div class="mb-3">
-          <label for="rodzaj" class="form-label">Rodzaj</label>
-          <select class="form-select" id="rodzaj" /> [(ngModel)]="rodzaj" name="rodzaj">
-          <option value="">Wybierz...</option>
-          <option value="1">Komedia</option>
-          <option value="2">Obyczajowy</option>
-          <option value="3">Sensacyjny</option>
-          <option value="4">Horror</option>
-        </select>
-      </></div><button type="button" class="btn btn-primary" /></></> (click)="dodajFilm()">Dodaj</button>
-  </form>
-
-
-
-
-
-
-
+        <div className="col-6">
+          <label htmlFor="rodzaj" className="form-label">Rodzaj</label>
+          <select id="rodzaj" className="form-select">
+            <option defaultValue={0}></option>
+            <option value="1">Komedia</option>
+            <option value="2">Obyczajowy</option>
+            <option value="3">Sensacyjny</option>
+            <option value="4">Horror</option>
+          </select>
+        </div><br></br>
+        <button type="submit" className="btn btn-primary">
+          Dodaj
+        </button>
+      </form>
     </div>
   );
 }
